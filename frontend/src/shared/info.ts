5cd0c50262ce1d947c8180e57961f0018ec59840
Sung Kwon <sung@phntms.com>
2020-11-08 15:46:00 +0000

    social

--- frontend/src/shared/info.ts
+++ frontend/src/shared/info.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
 
+const socialText =
+    'Share your mood during Covid-19 pandemic — https://covid-mood.world/';
 @Component({
   selector: 'app-info',
   templateUrl: './info.html',
   styleUrls: ['./info.scss']
 })
 export class InfoComponent {
+  readonly encodedUri = encodeURI(socialText);
 }
